d2c8e3631b8d715e2d722ba6fdff2a17e43d1a47
Lawrence Boadilla <lawrence@klab.com>
2013-06-27 09:31:13 +0800

    refactor based on PR

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -5,6 +5,17 @@ class ThreadController extends AppController
 	{
 		// TODO: Get all threads
 		$threads = Thread::getAll();
+
+		$adapter   = new \Pagerfanta\Adapter\ArrayAdapter($threads);
+		$paginator = new \Pagerfanta\Pagerfanta($adapter);
+		$paginator->setMaxPerPage(5);
+		$paginator->setCurrentPage(Param::get('page', 1));
+		$threads   = Thread::objectToarray($paginator);
+
+		$view    = new \Pagerfanta\View\TwitterBootstrapView();
+		$options = array('proximity' => 3, 'url' => 'card/all');
+		$html    = $view->render($paginator, 'routeGenerator', $options);
+
 		$this->set(get_defined_vars());
 	}
 
@@ -26,18 +37,17 @@ class ThreadController extends AppController
 			case 'write':
 			break;
 			case 'write_end':
-			$comment->username = Param::get('username');
-			$comment->body = Param::get('body');
-
-			try {
-				$thread->write($comment);
-			} catch (ValidationException $e) {
-				$page = 'write';
-			}
+				$comment->username = Param::get('username');
+				$comment->body = Param::get('body');
+				try {
+					$thread->write($comment);
+				} catch (ValidationException $e) {
+					$page = 'write';
+				}
 
 			break;
 			default:
-			throw new NotFoundException("{$page} is not found");
+				throw new NotFoundException("{$page} is not found");
 			break;
 		}
 
@@ -58,6 +68,7 @@ class ThreadController extends AppController
 				$thread->title = Param::get('title');
 				$comment->username = Param::get('username');
 				$comment->body = Param::get('body');
+				
 				try {
 					$thread->create($comment);
 				} catch (ValidationException $e) {
@@ -70,6 +81,34 @@ class ThreadController extends AppController
 		}
 		$this->set(get_defined_vars());
 		$this->render($page);
+	}
+
+	public function register()
+	{	
+		$thread         = new Thread;
+		$page           = Param::get('page_next', 'register');
+		$username       = Param::get('username');
+		$password       = Param::get('password');
+
+		switch ($page) {
+			case 'register':
+
+			break;
+
+			case 'register_end':
+				try {
+					$status = $thread->registerUser($username, $password);
+				} catch (ValidationException $e) {
+					
+				}
+			break;
+
+			default:
+				throw new NotFoundException("{$page} is not found");
+			break;
+		}
+		$this->render($page);
+		$this->set(get_defined_vars());
 	}
 
 }
--- app/models/thread.php
+++ app/models/thread.php
@@ -1,98 +1,108 @@
 <?php
 class Thread extends AppModel
 {
-	    public $validation = array(
-			'title' => array(
-				'length' => array(
-					'validate_between', 1, 30,
+	public $validation = array(
+		'title' => array(
+			'length' => array(
+				'validate_between', 1, 30,
 				),
 			),
 		);
 
-		public static function get($id)
-		{
-			$db = DB::conn();
-			$row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
-			return new self($row);
+	public static function get($id)
+	{
+		$db  = DB::conn();
+		$row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
+		return new self($row);
+	}
+
+	public static function getAll()
+	{
+		$threads = array();
+		$db = DB::conn();
+		$rows = $db->rows('SELECT * FROM thread');
+		foreach ($rows as $row) {
+			$threads[] = new Thread($row);
+		}
+		return $threads;
+	}
+
+	public function getComments()
+	{
+		$comments = array();
+		$db = DB::conn();
+		$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC LIMIT 20', array($this->id));
+		foreach ($rows as $row) {
+			$comments[] = new Comment($row);
 		}
 
+		return $comments;
+	}
 
-		public static function getAll()
-		{
-			$threads = array();
-			$db = DB::conn();
-			$rows = $db->rows('SELECT * FROM thread');
-			foreach ($rows as $row) {
-				$threads[] = new Thread($row);
-			}
-			return $threads;
+	public function getTestComments()
+	{
+		$comments = array();
+		$db = DB::conn();
+		$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC LIMIT 20', array($this->id));
+		foreach ($rows as $row) {
+			$comments[] = new Comment($row);
 		}
 
-		public function getComments()
-		{
-			$comments = array();
-			$db = DB::conn();
-			$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC LIMIT 20', array($this->id));
-			foreach ($rows as $row) {
-				$comments[] = new Comment($row);
-			}
+		return $comments;
+	}
 
-			return $comments;
+	public function write(Comment $comment)
+	{
+		if (!$comment->validate()) {
+			throw new ValidationException('invalid comment');
 		}
 
-		public function getTestComments()
-		{
-			$comments = array();
-			$db = DB::conn();
-			$rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC LIMIT 20', array($this->id));
-			foreach ($rows as $row) {
-				$comments[] = new Comment($row);
-			}
+		$db = DB::conn();
+		$db->query('INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()', array($this->id, $comment->username, $comment->body));
+	}
 
-			return $comments;
-		}
-
-		public function write(Comment $comment)
-		{
-			if (!$comment->validate()) {
-				throw new ValidationException('invalid comment');
-			}
-
-			$db = DB::conn();
-			$db->query('INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()', array($this->id, $comment->username, $comment->body));
-		}
-
-		public function create(Comment $comment)
-		{
-			$this->validate();
-			$comment->validate();
+	public function create(Comment $comment)
+	{
+		$this->validate();
+		$comment->validate();
 			if ($this->hasError() || $comment->hasError()) {
 				throw new ValidationException('invalid thread or comment');
 			}
-			$db = DB::conn();
-			$db->begin();
-			$db->query('INSERT INTO thread SET title = ?, created = NOW()', array($this->title));
-			$this->id = $db->lastInsertId();
-
-			// write first comment at the same time
-			$this->write($comment);
-			$db->commit();
-		}
-
-		public static function registerUser($username, $password){
-			$db     = DB::conn();
-			$status = "";
-			try {
-				$db->begin();
-				$db->query('INSERT INTO user SET user_name = ?, user_password = ?', array($username, $password));
-				$db->commit();
-				$status = "success";
-				return $status;
-			} catch (Exception $e) {
-				$status = "failed";
-				return $status;
-			}
-			
-		}
-
+		$db = DB::conn();
+		$db->begin();
+		$db->query('INSERT INTO thread SET title = ?, created = NOW()', array($this->title));
+		$this->id = $db->lastInsertId();
+		$this->write($comment);
+		$db->commit();
 	}
+
+	public static function registerUser($username, $password){
+		$db     = DB::conn();
+		try {
+			$db->query('INSERT INTO user SET user_name = ?, user_password = ?', array($username, $password));
+			$status = "success";
+		} catch (Exception $e) {
+			$status = "failed";
+		
+		}
+
+		return $status;
+	}
+
+	public static function objectToarray($data)
+	{
+		try{
+			if (is_array($data) || is_object($data)){
+				$result = array();
+				foreach ($data as $key => $value){
+					$result[$key] = $value;
+				}
+				return $result;
+			}
+			return $data;
+		} catch(PDOException $e) {
+			return "error ";
+		}
+	}
+
+}
